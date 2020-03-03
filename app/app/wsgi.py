bfd57b7275fb5f9858f6c532971c90e88a3beebe
wow1548 <wow1548@naver.com>
2020-03-03 15:40:48 +0900

    settings

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -16,4 +16,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/staticfiles")
+application = WhiteNoise(application)
